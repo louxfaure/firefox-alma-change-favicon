e34154cac54af50f907966ba25d8d316a2ce2270
louxfaure <louxfaure@gmail.com>
2024-09-27 18:28:41 +0200

    hh

--- change_alma_favicon.js
+++ change_alma_favicon.js
@@ -1,23 +1,31 @@
 
+(function() {
+  const domain = window.location.hostname;
 
-// Fonction pour changer le favicon
-function changeFavicon(faviconUrl) {
-  var link = document.querySelector("link[rel~='icon']");
-  if (!link) {
-      link = document.createElement('link');
-      link.rel = 'icon';
+  // Fonction pour changer le favicon
+  function changeFavicon(src) {
+      let link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+      link.type = 'image/x-icon';
+      link.rel = 'shortcut icon';
+      link.href = browser.extension.getURL(src);
       document.getElementsByTagName('head')[0].appendChild(link);
   }
-  link.href = browser.extension.getURL(faviconUrl);
-}
 
-// Obtenir l'URL de la page actuelle
-var currentUrl = window.location.href;
+  // Fonction pour changer le title
+  function changeTitle(text) {
+      let title = document.querySelector("title");
+      if (!title) {
+          title = document.createElement('title');
+          document.body.insertBefore(title, document.body.firstChild);
+      }
+      title.textContent = "Alma " + text;
+  }
 
 // Extraction du code de l'institution dans sous-domaine
-var match = currentUrl.match(/pudb-(\w+)\.alma/);
+var match = domain.match(/pudb-(\w+)\.alma/);
 if (match && match[1]) {
   var institution = match[1]; 
+  console.log(institution)
   
   // Chemin du favicon en fonction du code de l'institution
   var faviconUrl = 'icons/'+ institution + '.ico';
@@ -25,6 +33,8 @@ if (match && match[1]) {
   // Changer le favicon (la zone réseau garde l'icone Alma)
   if (institution != "network"){
     changeFavicon(faviconUrl);
+    changeTitle(institution);
   }
   
 }
+})();
